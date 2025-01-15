7d48e33a14d8858e1b81bc43677e6e8a0464c8f0
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-01-15 22:59:02 +0700

    💄slicing half card design

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const ProfileCard = () => {
-  return <div>ProfileCard</div>;
+  return (
+    <div className="flex justify-center h-screen items-center">
+      <div className="flex flex-col items-center justify-center p-10 bg-gray-700 rounded-md">
+        <img
+          src="https://randomuser.me/api/portraits/women/24.jpg"
+          className="rounded-full w-20 h-20"
+          alt="user icon"
+        />
+        <div className="font-semibold text-2xl text-white">Jessica Randali</div>
+        <div style={{ color: "hsl(75, 94%, 57%)" }}>London, United Kingdom</div>
+        <div className="text-white mt-4">
+          "Front-end developer and avid reader"
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileCard;
