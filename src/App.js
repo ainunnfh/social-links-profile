302b350cabcfd95a67b8804ad6882b8433c966c1
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-01-18 13:50:09 +0700

    🆙update (app): add child to profile card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import ProfileCard from "./components/ProfileCard.tsx";
+import SocmedAccounts from "./components/SocmedAccounts.tsx";
 function App() {
   return (
     <div>
-      <ProfileCard />
+      <ProfileCard
+        children={<SocmedAccounts name="Github" url="https://github.com/" />}
+      />
     </div>
   );
 }
