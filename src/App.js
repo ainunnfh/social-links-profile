3d83431471604efaf6d720f3e7e4a35e41c3b09b
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-01-18 14:27:47 +0700

    🆙update (app): create array of accounts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import React from "react";
 import "./App.css";
 import ProfileCard from "./components/ProfileCard.tsx";
-import SocmedAccounts from "./components/SocmedAccounts.tsx";
-function App() {
+
+const App = () => {
+  const socialAccounts = [
+    { platform: "Github", url: "https://www.github.com" },
+    { platform: "Frontend Mentor", url: "https://www.frontendmentor.io" },
+    { platform: "Linkedin", url: "https://www.linkedin.com" },
+    { platform: "Twitter", url: "https://www.twitter.com" },
+    { platform: "Instagram", url: "https://www.instagram.com" },
+  ];
+
   return (
     <div>
-      <ProfileCard
-        children={<SocmedAccounts name="Github" url="https://github.com/" />}
-      />
+      <ProfileCard accounts={socialAccounts} />
     </div>
   );
-}
+};
 
 export default App;
