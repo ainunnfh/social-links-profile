0c6c502f1507df6de5254cbb8c84aacdf2272ac5
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-01-18 13:49:50 +0700

    💄update (socmed acc): create github acc

--- src/components/SocmedAccounts.tsx
+++ src/components/SocmedAccounts.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const SocmedAccounts = () => {
-  return <div classneme="text-black flex bg-">SocmedAccounts</div>;
+interface ISocmedAcc {
+  name: string;
+  url?: string;
+}
+const SocmedAccounts = (props: ISocmedAcc) => {
+  return (
+    <div className="text-white text-center font-semibold flex bg-gray-700 w-64 p-2 rounded-md">
+      <div className="cursor-pointer">
+        <a href={props.url} target="_blank">
+          {props.name}
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default SocmedAccounts;
