951e61785cb0945d722e276cf7d838083a515f53
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-01-18 13:49:18 +0700

    🆙update (profile card): create children

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 
-const ProfileCard = () => {
+interface IProfileCardPropd {
+  children?: React.ReactNode;
+}
+const ProfileCard = (props: IProfileCardPropd) => {
   return (
-    <div className="flex justify-center h-screen items-center">
-      <div className="flex flex-col items-center justify-center p-10 bg-gray-700 rounded-md">
+    <div className="flex justify-center h-screen items-center ">
+      <div className="flex flex-col items-center justify-center p-10 bg-gray-800 rounded-md">
         <img
           src="https://randomuser.me/api/portraits/women/24.jpg"
           className="rounded-full w-20 h-20"
@@ -14,6 +17,7 @@ const ProfileCard = () => {
         <div className="text-white mt-4">
           "Front-end developer and avid reader"
         </div>
+        <div className="mt-4">{props.children}</div>
       </div>
     </div>
   );
