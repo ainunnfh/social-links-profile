3cd347c590af2ba00ab54925ed1091bd55c7d6f2
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-01-18 14:36:32 +0700

    💄update: change colors

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -6,16 +6,16 @@ interface IProfileCardPropd {
 }
 const ProfileCard = (props: IProfileCardPropd) => {
   return (
-    <div className="flex justify-center h-screen items-center">
-      <div className="flex flex-col items-center justify-center p-10 bg-gray-800 rounded-md">
+    <div className="flex justify-center h-screen items-center bg-[#141414]">
+      <div className="flex flex-col items-center justify-center p-10 bg-[#1f1f1f] rounded-md">
         <img
           src="https://randomuser.me/api/portraits/women/24.jpg"
           className="rounded-full w-20 h-20"
           alt="user icon"
         />
         <div className="font-semibold text-2xl text-white">Jessica Randali</div>
-        <div style={{ color: "hsl(75, 94%, 57%)" }}>London, United Kingdom</div>
-        <div className="text-white mt-4">
+        <div className="text-[#c5f82a]">London, United Kingdom</div>
+        <div className="text-white font-light mt-4">
           "Front-end developer and avid reader"
         </div>
 
